93353e0d7f0e8aab1df2ce1fabad933bbb16b01b
yusuke-totsuka <yusuke.totsuka@appent.jp>
2023-06-04 00:31:38 +0900

    いろいろ

--- back/index.js
+++ back/index.js
@@ -17,7 +17,7 @@ app.get("/get-list", (req, res) => {
   connection.query("SELECT * FROM testapi_table", (error, results) => {
     if (error) {
       console.error(error);
-      res.status(500).json({ error: "Internal server error" });
+      res.status(500).json({error: "Internal server error"});
     } else {
       res.json(results);
     }
@@ -29,19 +29,79 @@ app.get("/get-list", (req, res) => {
 app.post(
   "/insert-list",
   (req, res) => {
-    const { name, type } = req.body;
+    const {
+      pokemon,
+      ability,
+      nature,
+      item,
+      move1,
+      move2,
+      move3,
+      move4,
+      effort_h,
+      effort_a,
+      effort_b,
+      effort_d,
+      effort_s,
+      individual_h,
+      individual_a,
+      individual_b,
+      individual_c,
+      individual_d,
+      individual_s,
+    } = req.body;
 
     //データベースに挿入するクエリ
     //テンプレートリテラルわからなくなったらggr
-    const query = `INSERT INTO testapi_table (id, name, type) VALUES (null, '${name}', '${type}')`;
+    const query = `INSERT INTO testapi_table (
+      pokemon,
+      ability,
+      nature,
+      item,
+      move1,
+      move2,
+      move3,
+      move4,
+      effort_h,
+      effort_a,
+      effort_b,
+      effort_d,
+      effort_s,
+      individual_h,
+      individual_a,
+      individual_b,
+      individual_c,
+      individual_d,
+      individual_s,) 
+      VALUES (
+      '${pokemon}',
+      '${ability}',
+      '${nature}',
+      '${item}',
+      '${move1}',
+      '${move2}',
+      '${move3}\',
+      '${move4}',
+      '${effort_h}',
+      '${effort_a}',
+      '${effort_b}',
+      '${effort_d}',
+      '${effort_s}',
+      '${individual_h}',
+      '${individual_a}',
+      '${individual_b}',
+      '${individual_c}',
+      '${individual_d}',
+      '${individual_s}'
+      )`;
 
     //クエリ実行
     connection.query(query, (err, result) => {
       if (err) {
         console.error(err);
-        res.status(500).json({ error: "データ登録中にエラー" });
+        res.status(500).json({error: "データ登録中にエラー"});
       } else {
-        res.json({ message: "データが正常に登録" });
+        res.json({message: "データが正常に登録"});
       }
     });
   } //第二引数ここまで
